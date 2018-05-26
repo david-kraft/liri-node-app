8cfaa6cf600e280de00a243ea23ecd96537243e8
David Kraft <kraft153@umn.edu>
2018-05-26 13:02:44 -0500

    Incorporated instructor comment: added err to myTweets function

--- tweets.js
+++ tweets.js
@@ -1,6 +1,6 @@
 // 1. `node liri.js my-tweets` * This will show your last 20 tweets and when they were created at in your terminal/bash window.
 module.exports = function myTweets(client, divider) {
-    client.get("https://api.twitter.com/1.1/statuses/user_timeline.json?", "screen_name=DavidKraftSB, count=20", function (data) {
+    client.get("https://api.twitter.com/1.1/statuses/user_timeline.json?", "screen_name=DavidKraftSB, count=20", function (err, data) {
         console.log(divider + data + divider);
     })
 };
