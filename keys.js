254e1fd1129d94c3bad862b422e9645be997bb2d
David Kraft <34525272+david-kraft@users.noreply.github.com>
2019-04-03 22:24:31 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
+// Twitter keys
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -5,6 +6,7 @@ exports.twitter = {
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
+// Spotify keys
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
