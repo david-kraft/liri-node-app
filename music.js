59665f1b8e221855dbb21d07e7e5284e4549a491
David Kraft <34525272+david-kraft@users.noreply.github.com>
2019-04-03 22:42:36 -0500

    Update music.js

--- music.js
+++ music.js
@@ -1,10 +1,3 @@
-// 2. `node liri.js spotify-this-song '<song name here>'`
-//  * This will show the following information about the song in your terminal/bash window
-//  * Artist(s)
-//  * The song's name
-//  * A preview link of the song from Spotify
-//  * The album that the song is from
-//  * If no song is provided then your program will default to "The Sign" by Ace of Base.
 module.exports = function spotifyThisSong(spotify, divider, input) {
     if (!input) {
         input = "the sign";
